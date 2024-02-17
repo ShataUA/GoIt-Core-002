f2985f712495b141ac29e405a44fa711480eb237
Andrii Liubov <andrii.liubov@gmail.com>
2024-02-17 21:22:57 +0200

    added SecondName Address Email

--- src/classes.py
+++ src/classes.py
@@ -1,6 +1,7 @@
 from collections import UserDict
 from datetime import datetime, date
 import pickle
+import re
 
 
 class Field:
@@ -19,16 +20,57 @@ class Field:
 
     @value.setter
     def value(self, value):
-        if self.is_valid(value):
-            self.__value = value
-        else:
+        self.__value = value if self.is_valid(value) else None
+        if not self.is_valid(value):
             raise ValueError
 
     def __str__(self):
         return str(self.value)
 
 
-class Name(Field):
+class FirstName(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = None
+        self.value = value
+
+    @staticmethod
+    def is_valid(value):
+        if value.isalpha():
+            return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value if self.is_valid(value) else None
+        if not self.is_valid(value):
+            raise ValueError
+
+class SecondName(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = None
+        self.value = value
+
+    @staticmethod
+    def is_valid(value):
+        if value is None or value.isalpha():
+            return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value if (self.is_valid(value) and value is not None) else None
+        if not self.is_valid(value):
+            raise ValueError
+
+class Address(Field):
     def __init__(self, value):
         super().__init__(value)
 
@@ -62,18 +104,36 @@ class Birthday(Field):
 
     @value.setter
     def value(self, value):
-        if self.is_valid(value):
-            if value is not None:
-                self.__value = datetime.strptime(value, '%d-%m-%Y').date()
-            else:
-                self.__value = None
-        else:
+        self.__value = datetime.strptime(value, '%d-%m-%Y').date() if (self.is_valid(value) and value is not None) else None
+        if not self.is_valid(value):
             raise ValueError
+        
 
+class Email(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.__value = None
+        self.value = value
+
+    @staticmethod
+    def is_valid(value):
+        if value is None or re.match(r"\b[A-Za-z]{1,}[A-Za-z0-9._]{1,}@[A-Za-z0-9]+\.[A-Za-z]{2,}\b|[A-Za-z]{1,}[A-Za-z0-9._]{1,}@[A-Za-z0-9]+\.[A-Za-z]{2,}\b", value):
+            return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        self.__value = value if (self.is_valid(value) and value is not None) else None
+        if not self.is_valid(value):
+            raise ValueError
+        
 
 class Record:
     def __init__(self, name, birthday=None):
-        self.name = Name(name)
+        self.name = FirstName(name)
         self.phones = []
         self.birthday = Birthday(birthday)
 
